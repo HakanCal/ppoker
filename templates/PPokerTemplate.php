dd80d5f8ed47106dd08dfc607c5ad02a6e6364e5
HakanCal <80169576+HakanCal@users.noreply.github.com>
2022-01-08 16:15:16 +0100

    not complete

---
+++ templates/PPokerTemplate.php
@@ -0,0 +1,17 @@
+<form action="index.php?page=PPoker" method="post">
+    <input type="radio" name="ppoker_select" id="value1" value="1">
+    <label for="value1">1</label><br>
+    <input type="radio" name="ppoker_select" id="value2" value="2">
+    <label for="value2">2</label><br>
+    <input type="radio" name="ppoker_select" id="value3" value="3">
+    <label for="value3">3</label><br>
+    <input type="radio" name="ppoker_select" id="value5" value="5">
+    <label for="value5">5</label><br>
+    <input type="radio" name="ppoker_select" id="value8" value="8">
+    <label for="value8">8</label><br>
+    <input type="radio" name="ppoker_select" id="value13" value="13">
+    <label for="value13">13</label><br>
+    <input type="radio" name="ppoker_select" id="value20" value="20">
+    <label for="value20">20</label><br>
+    <input type="submit">
+</form>
--- views/PPoker.php
+++ views/PPoker.php
@@ -1,6 +1,30 @@
 <?php
 class PPoker extends Page implements PageInterface{
+    
+       
     function output(): string{
-        return "";
+        //Show GamePage if player did not select card
+        ob_start();
+        require "templates/Header.php";
+        //Check if user is logged in else redirect to login
+        if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true){
+            //TODO Allow game entry only when the requesting user is participant of this room
+            if($this->controller->isParticipant($_SESSION['userID'],$_GET['roomID'])){
+                if(!isset($_POST['ppoker_select'])){
+                    require "templates/PPokerTemplate.php";
+                }else{
+                    $this->model->setSelection($_SESSION['userID'], $_GET['roomID'], $_POST['ppoker_select']);
+                    $html = "Deine Wahl: ".$_POST['ppoker_select'];
+                }
+            }else{
+                echo "Du wurdest nicht zu diesem Spiel eingeladen!";
+            }
+        }else{
+            header('Location: index.php?page=Login');
+        }
+        require "templates/Footer.php";
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
     }
 }
